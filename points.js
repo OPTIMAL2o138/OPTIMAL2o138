4ef8916951cdbf1ef4a4df6d363f4fe1ef61452f
OPTIMAL2o138 <optimalcircuits5@gmail.com>
2023-10-26 16:05:18 +0530

    v.0.1.5

--- points.js
+++ points.js
@@ -1,1 +1,127 @@
-var pointJson = {"type":"FeatureCollection","features":[{"type":"Feature","properties":{"name":"ground 1","marker-color":"#7e7e7e","marker-size":"medium","marker-symbol":"circle"},"geometry":{"coordinates":[76.08946139334313,10.699134648136393],"type":"Point"},"id":0},{"type":"Feature","properties":{"name":"efefef","marker-color":"#7e7e7e","marker-size":"medium","marker-symbol":"circle"},"geometry":{"coordinates":[76.09002176832365,10.699258989657551],"type":"Point"},"id":1},{"type":"Feature","properties":{"name ":"xd","marker-color":"#7e7e7e","marker-size":"medium","marker-symbol":"circle"},"geometry":{"coordinates":[76.09020648854772,10.699558757013193],"type":"Point"},"id":2},{"type":"Feature","properties":{"name":"hello","marker-color":"#7e7e7e","marker-size":"medium","marker-symbol":"circle"},"geometry":{"coordinates":[76.08947969765381,10.699586779668891],"type":"Point"},"id":3},{"type":"Feature","properties":{"name":"shayan","marker-color":"#7e7e7e","marker-size":"medium","marker-symbol":"circle"},"geometry":{"coordinates":[76.089118391473,10.699328709469114],"type":"Point"},"id":4},{"type":"Feature","properties":{"name ":"hi","marker-color":"#7e7e7e","marker-size":"medium","marker-symbol":"circle"},"geometry":{"coordinates":[76.08909443114317,10.699679918975889],"type":"Point"},"id":5}]}
+var pointJson = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "LP block",
+        "stroke": "#555555",
+        "stroke-width": 2,
+        "stroke-opacity": 1,
+        "fill": "#555555",
+        "fill-opacity": 0.5
+      },
+      "geometry": {
+        "coordinates": [
+          [
+            [
+              76.08867912009504,
+              10.69935328888134
+            ],
+            [
+              76.08870307902146,
+              10.699033112113796
+            ],
+            [
+              76.08825265122152,
+              10.699009569690176
+            ],
+            [
+              76.0882670265774,
+              10.698913045738124
+            ],
+            [
+              76.08885402025129,
+              10.698943650897675
+            ],
+            [
+              76.08881568597138,
+              10.699357997360835
+            ],
+            [
+              76.08867912009504,
+              10.69935328888134
+            ]
+          ]
+        ],
+        "type": "Polygon"
+      },
+      "id": 0
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name ": "Vanaspathi ",
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "circle",
+        "venue": "computer lab 6",
+        "floor": "Ground"
+      },
+      "geometry": {
+        "coordinates": [
+          76.08871768784496,
+          10.699231829007545
+        ],
+        "type": "Point"
+      },
+      "id": 1
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "VANASPATHI",
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "circle",
+        "venue": "computer lab 6"
+      },
+      "geometry": {
+        "coordinates": [
+          76.08871543437067,
+          10.699097627991563
+        ],
+        "type": "Point"
+      },
+      "id": 2
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Vanaspathi",
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "circle",
+        "Venu": "computer lab 3",
+        "floor": "basement",
+      },
+      "geometry": {
+        "coordinates": [
+          76.08861736273872,
+          10.69897387753143
+        ],
+        "type": "Point"
+      },
+      "id": 3
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "Vanaspathi",
+        "marker-color": "#7e7e7e",
+        "marker-size": "medium",
+        "marker-symbol": "circle",
+        "venue": "computer lab 4",
+        "floor": "basement"
+      },
+      "geometry": {
+        "coordinates": [
+          76.08845976849813,
+          10.698980709353478
+        ],
+        "type": "Point"
+      },
+      "id": 4
+    }
+  ]
+}
